10b343772146f4c8d312bc44e3496f054504aaf0
Morena101 <ismonnanyana11@gmail.com>
2025-10-30 12:02:27 +0200

    added list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import pokemon from "./pokemon.json";
 
 function App() {
   return (
@@ -19,11 +20,12 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {/* Table rows will go here */}
-          <tr>
-            <td>Morena</td>
-            <td>Human</td>
+          {pokemon.slice(0, 20).map((pokemon) => (
+          <tr key={pokemon.id}>
+            <td>{pokemon.name.english}</td>
+            <td>{pokemon.type.join(", ")}</td>
           </tr>
+          ))}
         </tbody>
       </table>
     </div>
